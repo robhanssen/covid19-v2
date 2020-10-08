c4c4f2fa55db9fb0ccb63ef0ef188481b6b60fb3
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-10-08 08:02:14 -0400

    unit population work

--- covid19-global-deaths-unitpolulation.r
+++ covid19-global-deaths-unitpolulation.r
@@ -17,7 +17,7 @@ infinite = 10000
 #
 # import via web API
 #
-covidfile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+covidfile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 covid_raw <- read_csv(covidfile)
 
 countryinformation <- read_csv("worldpopulation/countryinformation.csv")
@@ -41,25 +41,23 @@ covid$location[is.na(covid$location)] = "Other"
 
 regionpopulation <- locations %>% inner_join(countryinformation) %>% group_by(location) %>% summarize(population=sum(population)) 
 
-
 # total spread of infections by countries
 spread <- covid %>% group_by(date, time, location) %>% summarise(count=sum(infections)) %>% arrange(location, time) %>% inner_join(regionpopulation)
 spread$count[spread$count==0] = 1e-1
 spread$per100k <- spread$count / spread$population * 100000
-# View(spread)
 #
 # clean up graphs and plot by country
 #
 capt = paste0(source, "\nlast updated:", format(lastupdated, format="%b %d, %Y"))
 
 spread %>% ggplot + aes(date, per100k, color=location) + geom_point()  + 
-                                        #scale_y_log10() + 
+                                        #scale_y_log10(limit=c(.1,1e3)) + 
                                         labs(caption=capt) + 
-                                        xlab("Date") + ylab("Infections") + ggtitle("Global spread of COVID-19 infections per 100k population") 
+                                        xlab("Date") + ylab("Deaths") + ggtitle("Global casualties of COVID-19 infections per 100k population") 
 
 
-# ggsave("graphs/covid-global-spread.pdf", device="pdf")
-# write_csv(spread, "data/covid-global-spread.csv")
+# ggsave("graphs/covid-global-deaths.pdf", device="pdf")
+# write_csv(spread, "data/covid-global-deaths.csv")
 
 # save data for later
 countryinfections <- spread
@@ -70,7 +68,6 @@ countryinfections <- spread
 #
 #
 #
-
 widespread <- spread %>% pivot_wider(names_from=location, values_from=count)
 covid_growth <- as_tibble(lapply(widespread[,1:ncol(widespread)],diff,lag=1))
 covid_growth$time = covid_growth$time + 1:NROW(covid_growth$time)
@@ -79,17 +76,17 @@ covid_growth$time = covid_growth$time + 1:NROW(covid_growth$time)
 covid_growth <- covid_growth %>% pivot_longer(!c("time","date"),
    names_to = "location",
    values_to = "growth") #%>% filter(location!="Other")
-
+View(covid_growth)
 covid_growth %>% mutate(date=as.Date("2020-01-22")+time) -> covid_growth
 
 covid_growth %>% ggplot + aes(date, growth, color=location) + geom_line(linetype="longdash") + #geom_smooth(method="loess") +
                         #scale_x_continuous() + 
                         labs(caption=capt) + 
-                        xlab("Date") + ylab("Growth of Infections") + ggtitle("Per diem growth of COVID-19 infections by country") +
+                        xlab("Date") + ylab("Growth of Infections") + ggtitle("Per diem growth of COVID-19 casualties by country") +
                         facet_wrap(.~location)
 
-# ggsave("graphs/covid-global-spreadgrowth.pdf", device="pdf")
-# write_csv(covid_growth, "data/covid-global-spreadgrowth.csv")
+# ggsave("graphs/covid-global-deathsgrowth.pdf", device="pdf")
+# write_csv(covid_growth, "data/covid-global-deathsgrowth.csv")
 
 # save data for later
 countrygrowth <- covid_growth
@@ -102,8 +99,7 @@ countrygrowth <- covid_growth
 #
 # assign locations to differentiate between counntries/groups of countries
 #
-# continents = read_csv("sources/countries-by-continent.csv")
-continents = read_csv("worldpopulation/countryinformation.csv") 
+continents = read_csv("sources/countries-by-continent.csv")
 covid <- covid_raw %>% inner_join(continents) %>% rename(location = continent)
 covid$location[is.na(covid$location)] = "Other"
 #View(covid)
@@ -118,13 +114,12 @@ spread$count[spread$count==0] = 1e-1
 
 capt = paste0(source, "\nlast updated:", format(lastupdated, format="%b %d, %Y"))
 
-spread %>%  filter(location != "Other") %>%
-            ggplot + aes(date, count, color=location) + geom_point()  + 
-                                        scale_y_log10(limit=c(1e3,1e7))  + labs(caption=capt) + 
-                                        xlab("Date") + ylab("Infections") + ggtitle("Spread of COVID-19 infections by continent") 
+spread %>% ggplot + aes(date, count, color=location) + geom_point()  + 
+                                        scale_y_log10(limit=c(1e2,1e6))  + labs(caption=capt) + 
+                                        xlab("Date") + ylab("Infections") + ggtitle("Spread of COVID-19 casualties by continent") 
                                         
-#  ggsave("graphs/covid-continents-spread.pdf", device="pdf")
-#  write_csv(spread, "data/covid-continents-spread.csv")
+#  ggsave("graphs/covid-continents-deaths.pdf", device="pdf")
+#  write_csv(spread, "data/covid-continents-deaths.csv")
 
 #
 #
@@ -144,17 +139,14 @@ covid_growth <- covid_growth %>% pivot_longer(!c("time","date"),
 
 covid_growth %>% mutate(date=as.Date("2020-01-22")+time) -> covid_growth
 
-covid_growth %>%  filter(location !="Other") %>%
-                  ggplot + aes(date, growth, color=location) + geom_line(linetype="longdash") + #geom_smooth(method="loess") +
+covid_growth %>% ggplot + aes(date, growth, color=location) + geom_line(linetype="longdash") + #geom_smooth(method="loess") +
                         #scale_x_continuous() + 
                         labs(caption=capt) + 
-                        xlab("Date") + ylab("Growth of Infections") + ggtitle("Per diem growth of COVID-19 infections by continent") +
+                        xlab("Date") + ylab("Growth of casualties") + ggtitle("Per diem growth of COVID-19 casualties by continent") +
                         facet_wrap(.~location)
 
-# ggsave("graphs/covid-continents-spreadgrowth.pdf", device="pdf", width=11, height=8.5)
-# write_csv(covid_growth, "data/covid-continents-spreadgrowth.csv")
+# ggsave("graphs/covid-continents-deathsgrowth.pdf", device="pdf")
+# write_csv(covid_growth, "data/covid-continents-deathsgrowth.csv")
 
 # save data for later
 countrygrowth <- covid_growth
-
- 
