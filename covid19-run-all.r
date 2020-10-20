5f5a28649a200407b7481bb3f7728fe54c9b9653
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-10-20 07:47:05 -0400

    per capita data

--- covid19-run-all.r
+++ covid19-run-all.r
@@ -8,3 +8,10 @@ source("covid19-global-infections.r")
 source("covid19-global-deaths.r")
 source("covid19-global-overview.r")
 source("covid19-usstates.r")
+
+#
+# per capita data
+#
+source("covid19-global-deaths-unitpopulation.r")
+source("covid19-global-infections-unitpopulation.r")
+source("covid19-usstates-unitpopulation.r")
