56e1ba42338dc590691cb53cb0d4ce8a30e29b92
Rob Hanssen <rob.hanssen@outlook.com>
2021-01-15 13:39:24 -0500

    update to heatmap

--- covid19-usstates-unitpopulation.r
+++ covid19-usstates-unitpopulation.r
@@ -80,18 +80,22 @@ ggsave("graphs-unitpopulation/covid-us-spreadgrowth-per100k.pdf", device="pdf", 
 # write_csv(covid_growth, "data/covid-us-spreadgrowth.csv")
 
 covid_growth1 <- covid_growth %>% mutate(week=week(date)) %>% group_by(location, week) %>% summarize(date=date,per100k=mean(per100k)) %>% ungroup() %>%
-                     mutate(levels = cut(per100k, breaks=c(-1,2,5,10,20, 1e5),labels=c("Safe","Impacted","Moderate","Severe","Critical")))
-                                 
+                     mutate(levels = cut(per100k, breaks=c(-1,2,5,10,20, 1e5),labels=c("Safe: 0-2 per 100k","Impacted: 2-5 per 100k","Moderate: 5-10 per 100k","Severe: 10-20 per 100k","Critical: >20 per 100k")))
+
+colorset = c(  "Safe: 0-2 per 100k" = "darkgreen", 
+               "Impacted: 2-5 per 100k" = "lightgreen", 
+               "Moderate: 5-10 per 100k"="yellow", 
+               "Severe: 10-20 per 100k"="orange",
+               "Critical: >20 per 100k"="red")
+
 
 covid_growth1 %>% ggplot()  + aes(x=location, y=date, fill=levels) + geom_tile() + coord_flip() + 
-                   scale_fill_manual(values = c("Safe" = "darkgreen", "Impacted" = "yellow", "Moderate"="pink", "Severe"="orange","Critical"="red")) + 
-                   scale_color_manual(values = c("Safe" = "darkgreen", "Impacted" = "yellow",  "Moderate"="pink", "Severe"="orange","Critical"="red")) + 
-                   labs(x="Region", y="Date", caption="Safe: 0-2 per 100k\nImpacted: 2-5 per 100k\nModerate: 5-10 per 100k\nSevere: 10-20 per 100k\n Critical: >20 per 100k")
+                   scale_fill_manual(values = colorset ) + 
+                   scale_color_manual(values = colorset ) + 
+                   labs(x="Region", y="Date") #, caption="Safe: 0-2 per 100k\nImpacted: 2-5 per 100k\nModerate: 5-10 per 100k\nSevere: 10-20 per 100k\n Critical: >20 per 100k")
+
 
 ggsave("graphs-unitpopulation/covid19-us-heat-map.pdf", width=11, height=8)
-
-
-
 
 
 # save variable for later use
